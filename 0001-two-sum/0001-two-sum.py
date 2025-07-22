d2618e1a2e40d3b2ede6990f139856ed995836b4
Harshinibattula <86304216+Harshinibattula@users.noreply.github.com>
2025-07-22 08:47:33 +0530

    Delete 0001-two-sum directory

--- 0001-two-sum/0001-two-sum.py
+++
@@ -1,8 +0,0 @@
-#O(n^2)
-class Solution:
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            x = target - nums[i]
-            for j in range(i + 1, len(nums)):
-                if nums[j] == x:
-                    return [i, j]
